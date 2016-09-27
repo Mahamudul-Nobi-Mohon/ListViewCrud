d4bc125b7c4b60dce661203d4148ea126f5f5802
mohon <mn.mohon@gmail.com>
2016-09-27 21:11:21 +0600

    New Toppic Added

---
+++ ListView.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ListView.aspx.cs" Inherits="ListView" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <div>
+        <fieldset style="width: 300px">
+            <legend>ListView Example</legend>
+            <asp:Table runat="server">
+                <asp:TableRow>
+                    <asp:TableCell>Name</asp:TableCell><asp:TableCell>
+                        <asp:TextBox runat="server" ID="txtName"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Contact</asp:TableCell><asp:TableCell>
+                        <asp:TextBox runat="server" ID="txtContact"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Email</asp:TableCell><asp:TableCell>
+                        <asp:TextBox runat="server" ID="txtEmail"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell></asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Button runat="server" ID="btnSave" Text="Save" OnClick="btnSave_Click" />
+                        <asp:Button runat="server" ID="btnUpdate" Text="Update" OnClick="btnUpdate_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </fieldset>
+    </div>
+
+    <asp:HiddenField ID="hfRecord" runat="server" />
+
+    <h3>Employee Information</h3>
+
+    <asp:ListView ID="ListEmployee" runat="server" OnItemCommand="ListEmployee_ItemCommand" DataKeyNames="EmpID">
+        <ItemTemplate>
+            <table>
+                <tr>
+                    <td style="width: 100px">
+                        <b>No. :</b>
+                        <%#Eval("EmpID") %>  
+                    </td>
+                    <td style="width: 200px">
+                        <b>Name :</b>
+                        <%#Eval("EmpName") %>  
+                    </td>
+                    <td style="width: 200px">
+                        <b>Contact :</b>
+                        <%#Eval("Contact") %>  
+                    </td>
+                    <td style="width: 250px">
+                        <b>Email :</b>
+                        <%#Eval("EmailId") %>  
+                    </td>
+                    <td>
+                        <asp:Button ID="btndel" runat="server" Text="Delete" ToolTip="Delete a record" OnClientClick="javascript:return confirm('Are you sure to delete record?')" CommandName="EmpDelete" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "EmpID") %>' />
+                        <asp:Button ID="btnupdt" runat="server" Text="Edit" ToolTip="Update a record" CommandName="EmpEdit" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "EmpID") %>' />
+                    </td>
+                </tr>
+            </table>
+        </ItemTemplate>
+    </asp:ListView>
+</asp:Content>
+
---
+++ ListView.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ListView : System.Web.UI.Page
+{
+    string connection = ConfigurationManager.ConnectionStrings["connstr"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GetEmployeeDetail();
+        }
+    }
+    void GetEmployeeDetail()
+    {
+        SqlConnection con = new SqlConnection(connection);
+
+        SqlCommand cmd = new SqlCommand("sp_FillData", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        con.Open();
+        ListEmployee.DataSource = cmd.ExecuteReader();
+        ListEmployee.DataBind();
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection con = new SqlConnection(connection))
+        {
+            using (SqlCommand cmd = new SqlCommand("sp_InsertEmployeeData", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.Parameters.AddWithValue("@EmpName", SqlDbType.VarChar).Value = txtName.Text.Trim();
+                cmd.Parameters.AddWithValue("@Contact", SqlDbType.NChar).Value = txtContact.Text.Trim();
+                cmd.Parameters.AddWithValue("@EmailId", SqlDbType.NVarChar).Value = txtEmail.Text.Trim();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                txtName.Text = String.Empty;
+                txtContact.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+            }
+        }
+        GetEmployeeDetail();
+    }
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(connection);
+        SqlCommand cmd = new SqlCommand("sp_UpdateEmployeeData", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        cmd.Parameters.AddWithValue("@EmpID", Convert.ToInt32(hfRecord.Value));
+        cmd.Parameters.AddWithValue("@EmpName", SqlDbType.VarChar).Value = txtName.Text.Trim();
+        cmd.Parameters.AddWithValue("@Contact", SqlDbType.NChar).Value = txtContact.Text.Trim();
+        cmd.Parameters.AddWithValue("@EmailId", SqlDbType.NVarChar).Value = txtEmail.Text.Trim();
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+
+        GetEmployeeDetail();
+
+        btnSave.Visible = true;
+        btnUpdate.Visible = false;
+
+        hfRecord.Value = string.Empty;
+        txtName.Text = String.Empty;
+        txtContact.Text = String.Empty;
+        txtEmail.Text = String.Empty;
+    }
+    protected void ListEmployee_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        switch (e.CommandName)
+        {
+            case ("EmpDelete"):
+                int EmpID = Convert.ToInt32(e.CommandArgument);
+                deleteEmployee(EmpID);
+                break;
+            case ("EmpEdit"):
+                EmpID = Convert.ToInt32(e.CommandArgument);
+                UpdateEmployeeDetail(EmpID);
+                break;
+        }
+    }
+    void deleteEmployee(int EmpID)
+    {
+        SqlConnection con = new SqlConnection(connection);
+        SqlCommand cmd = new SqlCommand("sp_DeleteEmployeeData", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        cmd.Parameters.AddWithValue("@EmpID", EmpID);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        GetEmployeeDetail();
+    }
+    void UpdateEmployeeDetail(int EmpID)
+    {
+        SqlConnection con = new SqlConnection(connection);
+        SqlCommand cmd = new SqlCommand("sp_SelectEmployeeData", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@EmpID", EmpID);
+
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        if (dr.HasRows)
+        {
+            dr.Read();
+            hfRecord.Value = dr["EmpID"].ToString();
+            txtName.Text = dr["EmpName"].ToString();
+            txtContact.Text = dr["Contact"].ToString();
+            txtEmail.Text = dr["EmailId"].ToString();
+        }
+        dr.Dispose();
+
+        con.Close();
+        btnSave.Visible = false;
+        btnUpdate.Visible = true;
+        GetEmployeeDetail();
+    }
+}
